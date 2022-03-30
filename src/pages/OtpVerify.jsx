d6f6f1f1228bec8718a54cf491deb0b5176f3685
Naveentn24 <naveensaven@gmail.com>
2022-03-30 11:57:53 +0530

    Price component updated

--- src/pages/OtpVerify.jsx
+++ src/pages/OtpVerify.jsx
@@ -1,22 +1,41 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import Topic from "../components/Topic";
 
 function OtpVerify() {
+  let location = useLocation();
   let navigate = useNavigate();
   const handleSubmit = () => {
     navigate("/submission");
   };
+  const { source, destination, carType, totalMembers } = location.state.values;
   return (
     <div>
       <Topic name={"Place your bid (3/4 step)"} />
       <div className="container">
         <form action="" className="bidForm ">
+        <div className="journey">
+            <div className="journey-header">
+              <span className="title">JOURNEY DETAILS</span>
+              {/* <Button name={"edit"} /> */}
+            </div>
+            <div className="journey-details">
+              <span>
+                {source} - {destination}
+              </span>{" "}
+              <br />
+              <span>
+                {totalMembers} Members,{carType}
+              </span>
+            </div>
+            {location.state.amount}
+          </div>
           <div className="form-input">
             <label htmlFor="otp">Verify OTP</label> <br />
             <input type="number" name="otp" id="otp" />
           </div>
+          
           <Button handleClick={handleSubmit} name={"Verify via OTP"} />
         </form>
       </div>
--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -4,17 +4,64 @@ import Button from "../components/Button";
 import Topic from "../components/Topic";
 
 function Price() {
-  const [amount, setAmount] = useState(0);
-
-  const handleChange = (e) => {
-    setAmount(e.target.value);
-  };
 
   let location = useLocation();
   let navigate = useNavigate();
-  const handleSubmit = () => {
-    navigate("/otp-verify");
+
+  const [amount, setAmount] = useState(0);
+  const [mobile, setMobile] = useState('');
+  const [cusName, setCusName] = useState('');
+  const [amountVal, setAmountVal] = useState('');
+  const [mobileVal, setMobileVal] = useState('');
+  const [nameVal, setNameVal] = useState('');
+  const [isDisabled, setDisabled] = useState(true);
+
+  const handleChange = (e) => {
+    const {name,value} = e.target;
+    if (name === 'amount'){
+      setAmount(value);
+    }else if(name === 'mobile'){
+      setMobile(value)
+    }else{
+      setCusName(value)
+    }
+    
   };
+
+  const validateAll =() => {
+    let isValid = true;
+    if (amount <= 0) {
+      setAmountVal('Please enter the amount');
+      isValid = false;
+    }
+
+    return isValid
+  }
+
+  const validateOne = (e) => {
+    const {name,value} = e.target;
+    if (name === 'amount' && value <= 0 ) {
+      setAmountVal('Amount is required');
+    }
+    if(name === 'mobile' && value.length <= 9){
+      setMobileVal('Enter 10 digit Mobile Number')
+    }
+    if(name === 'cusName' && (value.length == 0 || value == '' || value == null )){
+      setNameVal('Enter the name')
+    }
+ 
+  }
+  const handleSubmit = () => {
+    const isValid = validateAll();
+    if (!isValid) {
+      return false;
+    }
+    setDisabled(false);
+  };
+
+  const handleSubmitOTP = () => {
+    navigate("/otp-verify",{state: {values:location.state,amount:amount}});
+  }
 
   const { source, destination, carType, totalMembers } = location.state;
   return (
@@ -47,15 +94,49 @@ function Price() {
                 id="amount"
                 value={amount}
                 onChange={handleChange}
+                onBlur={validateOne}
               />
             </div>
+            <div className="errors">{amountVal}</div>
             <div>
               <input type="checkbox" name="rate" id="rate"/>
               <span>Rate Negiotable</span>
             </div>
           </div>
 
-          <Button handleClick={handleSubmit} name={"Next"} />
+          {isDisabled && <Button handleClick={handleSubmit} name={"Next"} />}
+
+          {!isDisabled && 
+            <div>
+              <div className="form-input">
+            <label htmlFor="mobile">Mobile Number</label> <br />
+            <input
+              type="number"
+              name="mobile"
+              id="mobile"
+              value={mobile}
+              onChange={handleChange}
+              onBlur={validateOne}
+            />
+            <div className="errors">{mobileVal}</div>
+          </div>
+          <div className="form-input">
+            <label htmlFor="cusName">Name</label> <br />
+            <input
+              type="text"
+              name="cusName"
+              id="cusName"
+              value={cusName}
+              onChange={handleChange}
+              onBlur={validateOne}
+            />
+            <div className="errors">{nameVal}</div>
+          </div>
+              <Button handleClick={handleSubmitOTP} name={"Verify OTP"} />
+            </div>}
+
+
+          
         </form>
       </div>
     </div>
